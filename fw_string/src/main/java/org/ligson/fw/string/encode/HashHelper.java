96e2181f02208c7a445c957cbf58400c94b64041
ligson <lijinsheng@lecxe.com>
2016-05-18 16:48:44 +0800

    增加字节直接哈希的方法

--- fw_string/src/main/java/org/ligson/fw/string/encode/HashHelper.java
+++ fw_string/src/main/java/org/ligson/fw/string/encode/HashHelper.java
@@ -12,17 +12,30 @@ import java.security.MessageDigest;
  */
 public class HashHelper {
 
+    public static byte[] hash(byte[] source, String algorithm) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            digest.update(source);
+            return digest.digest();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public static String hash(String source, String algorithm, String charset) {
         try {
             byte[] buffer = source.getBytes(charset);
-            MessageDigest digest = MessageDigest.getInstance(algorithm);
-            digest.update(buffer);
-            byte[] bytes = digest.digest();
+            byte[] bytes = hash(buffer, algorithm);
             return Hex.encodeHexString(bytes).toUpperCase();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static byte[] md5(byte[] source) {
+        return hash(source, "MD5");
     }
 
     public static String md5(String source, String charset) {
